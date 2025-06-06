03ca6888ecd4f131c92dfd4f4e346667bb4360a1
Keyur R. Siddhapura <103017634+CapK13@users.noreply.github.com>
2025-06-06 11:47:17 +0530

    Update app.js
    makin new

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const dotenv = require('dotenv');
+// const express = require('express');
+// const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
 var conn = null;
